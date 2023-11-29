0bdfb2ce9b109cb769671603bd69fa9e670ba23a
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-11-29 17:28:50 +0000

    Update and rename FindVerticalRegen to FindVerticalRegen.lua

--- FindVerticalRegen.lua
+++ FindVerticalRegen.lua
@@ -1,8 +1,10 @@
 local root = game.Players.LocalPlayer.Character.HumanoidRootPart
 root.Anchored = true
+
 repeat
-	wait(.15)
-	root.CFrame = CFrame.new(-7.165, root.Position.Y + 2500 , 94.743)
+wait(.15)
+root.CFrame = CFrame.new(-7.165, root.Position.Y + 2500 , 94.743)
 until workspace.Terrain._Game.Admin:FindFirstChild("Regen")
+
 root.Anchored = false
 root.CFrame = workspace.Terrain._Game.Admin:FindFirstChild("Regen").CFrame + Vector3.new(0, 3, 0)
