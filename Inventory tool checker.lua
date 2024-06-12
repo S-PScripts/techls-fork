7ca3c71d896b326d4d475c22edec4a892af46409
S-PScripts <ts2021me@outlook.com>
2024-06-12 18:04:02 +0100

    Update Inventory tool checker.lua

--- Inventory tool checker.lua
+++ Inventory tool checker.lua
@@ -1,14 +1,27 @@
-local prefix = "!"
+function checkforplayer(plr)
+    for i, v in pairs(game.Players:GetPlayers()) do
+        if string.sub(v.Name:lower(), 1, #plr) == plr then
+            print(plr .. " was found as " .. v.Name)
+            return v
+        end
+    end
+    return nil
+end
 
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
-    	if string.sub(msg:lower(), 1, #prefix + 3) == prefix.."cbp" then
-		local player = string.sub(msg, #prefix + 5, #msg) -- put the full username
-		local gplayer = game.Players:FindFirstChild(player)
-		local Backpack = gplayer:FindFirstChild("Backpack")
-		if Backpack then
-			for _, v in pairs(Backpack:GetChildren()) do
-				print(v.Name)
-			end
-		end
+        if string.sub(msg:lower(), 1, 14) == "!checkbackpack" then
+            local finding = string.sub(msg:lower(), 16)
+            if not checkforplayer(finding) then
+                print("Invalid player")
+                return
+            end
+            local wtech = checkforplayer(finding)
+            local gplayer = game.Players:FindFirstChild(wtech)
+            local Backpack = gplayer:FindFirstChild("Backpack")
+            if Backpack then
+                for _, v in pairs(Backpack:GetChildren()) do
+                    print(v.Name)
+                end
+            end
         end
 end)
